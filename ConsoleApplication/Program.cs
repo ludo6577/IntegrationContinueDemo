e67b80f1573553bc022a52a1bc935bd8c6b102a0
Ludovic Feltz <ludovic.feltz@softfluent.com>
2016-03-23 16:57:08 +0100

    Init project

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fibo(5));
+            Console.ReadLine();
+        }
+        public static int Fibo(int n)
+        {
+            return FiboImpl1(n);
+        }
+        public static int FiboImpl1(int n)
+        {
+            if (n < 0)
+                throw new ArgumentException("Argument cannot be negative");
+
+            if (n <= 1)
+                return n;
+            return FiboImpl1(n - 1) + FiboImpl1(n - 2);
+        }
+        public static int FiboImpl2(int n)
+        {
+            if (n < 0)
+                throw new ArgumentException("Argument cannot be negative");
+
+            int a = 0, b = 1, c = n;
+            for (int i = 2; i <= n; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            return c;
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using ConsoleApplication;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestFibo0()
+        {
+            Assert.AreEqual(0, Program.Fibo(0));
+        }
+
+        [TestMethod]
+        public void TestFibo1()
+        {
+            Assert.AreEqual(1, Program.Fibo(1));
+        }
+
+        [TestMethod]
+        public void TestFibo5()
+        {
+            Assert.AreEqual(5, Program.Fibo(5));
+        }
+
+        [TestMethod]
+        public void TestFibo13()
+        {
+            Assert.AreEqual(233, Program.Fibo(13));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "A userId of null was inappropriately allowed.")]
+        public void TestFiboNegatif()
+        {
+            Program.Fibo(-1);
+        }
+    }
+}
